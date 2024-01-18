13c49bd1c9c2de83fd3d3a6946d8e9a1b162eb5b
lebiosphere <lebiosphere@gmail.com>
2024-01-18 10:36:05 +0100

    Correction des adresses

--- main.ts
+++ main.ts
@@ -1,18 +1,20 @@
 input.onButtonPressed(Button.A, function () {
-    adresse = Math.min(1, adresse - 1)
+    adresse = Math.max(0, adresse - 1)
+    basic.pause(500)
 })
 input.onButtonPressed(Button.B, function () {
-    adresse = Math.max(25, adresse + 1)
+    adresse = Math.min(24, adresse + 1)
+    basic.pause(500)
 })
 let adresse = 0
 radio.setGroup(43)
-adresse = 1
+adresse = 0
 basic.forever(function () {
     if (grove.measureInCentimetersV2(DigitalPin.P0) > 4) {
-        radio.sendNumber(3)
+        radio.sendNumber(adresse)
         basic.showIcon(IconNames.Skull)
     } else {
-        basic.showString("" + (adresse))
+        basic.showString("" + (adresse + 1))
     }
     basic.pause(200)
 })
