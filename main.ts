d36eb776fd15e5fa40309b6a421d6890e549c0c5
lebiosphere <lebiosphere@gmail.com>
2024-01-18 10:28:26 +0100

    Modifier l'adresse des chiottes avec A et B

--- main.ts
+++ main.ts
@@ -1,10 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    adresse = Math.min(1, adresse - 1)
+})
+input.onButtonPressed(Button.B, function () {
+    adresse = Math.max(25, adresse + 1)
+})
+let adresse = 0
 radio.setGroup(43)
+adresse = 1
 basic.forever(function () {
     if (grove.measureInCentimetersV2(DigitalPin.P0) > 4) {
         radio.sendNumber(3)
         basic.showIcon(IconNames.Skull)
     } else {
-        basic.showIcon(IconNames.Yes)
+        basic.showString("" + (adresse))
     }
     basic.pause(200)
 })
